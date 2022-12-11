df0d5760490e9290c628344f5436c47f3680a96b
nascript <nascodefy@gmail.com>
2022-12-12 01:37:33 +0700

    init project

---
+++ src/test/java/com/springboot_journey/nascript/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_journey.nascript.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
